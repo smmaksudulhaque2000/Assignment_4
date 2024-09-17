750d8b359b9722eb125d091f55ccd5cb62639a9d
maksudulhaque <smmaksudulhaque2000@gmail.com>
2024-09-17 16:30:16 +0600

    Un Comment Other Function

--- Assignment_4.js
+++ Assignment_4.js
@@ -1,41 +1,41 @@
-// function calculateTax(income, expense) {
-//   if (0 <= income && 0 <= expense && income >= expense) {
-//     let tax = (income - expense) * 0.2;
-//     return tax;
-//   } else {
-//     return "Invalid Input";
-//   }
-// }
+function calculateTax(income, expense) {
+  if (0 <= income && 0 <= expense && income >= expense) {
+    let tax = (income - expense) * 0.2;
+    return tax;
+  } else {
+    return "Invalid Input";
+  }
+}
 
-// function sendNotification(email) {
-//   if (email.includes("@") === true) {
-//     let nameAndDomain = email.split("@");
-//     return nameAndDomain[0] + " sent you an email from " + nameAndDomain[1];
-//   } else {
-//     return "Invalid Email";
-//   }
-// }
+function sendNotification(email) {
+  if (email.includes("@") === true) {
+    let nameAndDomain = email.split("@");
+    return nameAndDomain[0] + " sent you an email from " + nameAndDomain[1];
+  } else {
+    return "Invalid Email";
+  }
+}
 
-// function checkDigitsInName(name) {
-//   if (typeof name !== "string") {
-//     return "Invalid Input";
-//   }
-//   if (
-//     name.includes(1) ||
-//     name.includes(2) ||
-//     name.includes(3) ||
-//     name.includes(4) ||
-//     name.includes(5) ||
-//     name.includes(6) ||
-//     name.includes(7) ||
-//     name.includes(8) ||
-//     name.includes(9)
-//   ) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function checkDigitsInName(name) {
+  if (typeof name !== "string") {
+    return "Invalid Input";
+  }
+  if (
+    name.includes(1) ||
+    name.includes(2) ||
+    name.includes(3) ||
+    name.includes(4) ||
+    name.includes(5) ||
+    name.includes(6) ||
+    name.includes(7) ||
+    name.includes(8) ||
+    name.includes(9)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 function calculateFinalScore(obj) {
   if (
@@ -60,17 +60,17 @@ function calculateFinalScore(obj) {
 console.log(calculateFinalScore("demo"));
 
 
-// function waitingTime(waitingTimes, serialNumber) {
-//   let sum = 0;
-//   if (Array.isArray(waitingTimes) && typeof serialNumber === "number") {
-//     for (let number of waitingTimes) {
-//       sum = sum + number;
-//     }
-//     let average = Math.round(sum / waitingTimes.length);
-//     let dueSerial = serialNumber - (waitingTimes.length + 1);
-//     let time = dueSerial * average;
-//     return time;
-//   } else {
-//     return "Invalid Input";
-//   }
-// }
+function waitingTime(waitingTimes, serialNumber) {
+  let sum = 0;
+  if (Array.isArray(waitingTimes) && typeof serialNumber === "number") {
+    for (let number of waitingTimes) {
+      sum = sum + number;
+    }
+    let average = Math.round(sum / waitingTimes.length);
+    let dueSerial = serialNumber - (waitingTimes.length + 1);
+    let time = dueSerial * average;
+    return time;
+  } else {
+    return "Invalid Input";
+  }
+}
